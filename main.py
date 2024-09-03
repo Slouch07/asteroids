501534c73abf1457b83d02ef788d33aea7dc5c0d
Ryan Bowler <slouchh07@gmail.com>
2024-09-02 17:26:22 -0700

    create virtual development environment and a main.py boilerplate for project

---
+++ main.py
@@ -0,0 +1,13 @@
+# imports the open-source pygame library
+import pygame
+from constants import *
+
+def main():
+  print("Starting asteroids!")
+  print(f"Screen width: {SCREEN_WIDTH}")
+  print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+# ensures the main() function is only called when this file is run directly.
+if __name__ == "__main__":
+  main()
