f84d441943c022298bbf9563a1bfae5f78dcb87a
Ryan Bowler <slouchh07@gmail.com>
2024-09-04 18:15:25 -0700

    add new player.py and circleshape.py files - both contain new code that will draw the triangle   that acts as the player for the game. - update main.py so that the player is drawn to the   screen.

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 # imports the open-source pygame library
 import pygame
 from constants import *
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+from player import *
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    fps_clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
 
 
+        # limit the framerate to 60 FPS
+        dt = fps_clock.tick(60) / 1000
 
 # ensures the main() function is only called when this file is run directly.
 if __name__ == "__main__":
