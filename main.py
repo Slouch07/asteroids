84d7ab824a721b1c27a5391839a27deb564b9e73
Ryan Bowler <slouchh07@gmail.com>
2024-09-07 17:34:49 -0700

    add logic for shooting asteroids - added new constants for the player and shot classes - created new shot classes

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 # imports the open-source pygame library
 import pygame
+import sys
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
+
     pygame.init()
     fps_clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid = AsteroidField()
+
+    Shot.containers = (shots, updatable, drawable)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -16,7 +34,18 @@ def main():
                 return
 
         pygame.Surface.fill(screen, (0, 0, 0))
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        for obj in updatable:
+            obj.update(dt)
+
+        for obj in asteroids:
+            if obj.check_collision(player):
+                print("Game Over!")
+                sys.exit()
+
         pygame.display.flip()
 
 
